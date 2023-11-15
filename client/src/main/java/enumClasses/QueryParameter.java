ea9de50da3e19c9cf67d69fb0473938ed0110ef5
MattyMar234 <103828861+MattyMar234@users.noreply.github.com>
2023-11-15 17:02:30 +0100

    modifica nomi

--- client/src/main/java/enumClasses/QueryParameter.java
+++ client/src/main/java/enumClasses/QueryParameter.java
@@ -1,38 +1,38 @@
 package enumClasses;
 
 
-public enum QueryParameter {
-    NAME("nome"),
-    USERNAME("cognome"),
-    USER_ID("user_id"),
-    CODICE_FISCALE("codice_fiscale"),
-    EMAIL("email"),
-    PASSWORD("password"),
-    CIVIC_NUMBER("civic_number"),
-    VIA_PIAZZA("piazza"),
-    CAP("cap"),
-    COMMUNE("comune"),
-    PROVINCE("provincia"),
-    LIMIT("limit"),
-    OFFSET("offset"),
-    THRESHOLD("threshold"),
-    SEARCH_STRING("search_string"),
-    IDS("ids"),
-    ALBUM_ID("album_id"),
-    ACCOUNT_ID("account_id"),
-    PLAYLIST_NAME("playlist_name"),
-    PLAYLIST_ID("playlist_id"),
-    SONG_ID("song_id"),
-    NEW_NAME("nuovo_name"),
-    COMMENT("commento"),
-    COMMENT_ID("id_commento");
-
+public enum QueryParameter 
+{    
+    NAME("Nome"),
+    USERNAME("Cognome"),
+    USER_ID("UserId"),
+    CODICE_FISCALE("Codice_fiscale"),
+    EMAIL("Email"),
+    PASSWORD("Password"),
+    CIVIC_NUMBER("CivicNumber"),
+    VIA_PIAZZA("Piazza"),
+    CAP("CAP"),
+    COMMUNE("Comune"),
+    PROVINCE("Provincia"),
+    LIMIT("Limit"),
+    OFFSET("Offset"),
+    THRESHOLD("Threshold"),
+    SEARCH_STRING("SearchString"),
+    IDS("IDs"),
+    ALBUM_ID("AlbumID"),
+    ACCOUNT_ID("AccountID"),
+    PLAYLIST_NAME("PlaylistName"),
+    PLAYLIST_ID("PlaylistID"),
+    SONG_ID("SongID"),
+    NEW_NAME("NewName"),
+    COMMENT("Commento"),
+    COMMENT_ID("CommentID");
 
 
     private String text;
 
-    QueryParameter(String...str) {
-        this.text = str[0];
+    QueryParameter(String str) {
+        this.text = str;
     }
 
     
